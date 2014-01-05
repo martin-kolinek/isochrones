b9aec452d5b4a8a74d009f973b026c3e58e7b4ce
Martin Kolinek <martin.kolinek@gmail.com>
2014-01-05 16:40:53 +0100

    Speeding up finding of the covered set
    ref #69

--- src/main/scala/org/isochrone/visualize/AreaCacheComponent.scala
+++ src/main/scala/org/isochrone/visualize/AreaCacheComponent.scala
@@ -9,6 +9,7 @@ import org.isochrone.ArgumentParser
 import org.isochrone.OptionParserComponent
 import scopt.OptionParser
 import com.typesafe.scalalogging.slf4j.Logging
+import slick.jdbc.StaticQuery.interpolation
 
 trait AreaCacheComponent {
     self: GraphComponentBase =>
@@ -29,15 +30,15 @@ trait DbAreaCacheComponent extends AreaCacheComponent with GraphComponentBase {
 
         def getNodesAreas(nds: Seq[NodeType]): Map[NodeType, List[NodeArea]] = {
             logger.debug(s"Retrieving nodes, count = ${nds.size}")
-            val q = for {
-                a1 <- roadNetTables.roadAreas if a1.nodeId.inSet(nds)
-                a2 <- roadNetTables.roadAreas if a2.id === a1.id
-                a3 <- roadNetTables.roadAreas if a3.nodeId === a2.nodeId
-            } yield (a3.nodeId, a3.id, a3.costToCover)
-            logger.debug(s"Query = ${q.selectStatement}")
+            val ndList = "(" + nds.mkString(",") + ")"
+            val q = sql"""select a2.node_id, a2.id, a2.cost_to_cover 
+                from (select distinct id from "#${roadNetTables.roadAreas.tableName}" where node_id in #$ndList) a1
+                    inner join "#${roadNetTables.roadAreas.tableName}" a2 on a1.id = a2.id 
+                order by a2.id""".as[(Long, Long, Double)]
             implicit val s = session
+            logger.debug("Before retrieving")
             val retr = q.list
-            logger.debug("Done retrieving")
+            logger.debug(s"Done retrieving (retrieved ${retr.size} rows)")
             retr.groupBy(_._1).map {
                 case (ndid, lst) => ndid -> (for ((_, aid, cst) <- lst) yield NodeArea(aid, cst))
             }.withDefaultValue(Nil)
