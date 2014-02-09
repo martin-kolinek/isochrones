a1047eedfc6827317dd4d95d2c7d3f64f01e3d11
Martin Kolinek <martin.kolinek@gmail.com>
2014-02-09 13:17:08 +0100

    Tests for tree contraction for hh
    ref #71

--- src/main/scala/org/isochrone/hh/TreeContractionPhase.scala
+++ src/main/scala/org/isochrone/hh/TreeContractionPhase.scala
@@ -8,23 +8,44 @@ import com.vividsolutions.jts.geom.Geometry
 import scala.annotation.tailrec
 
 trait TreeContraction {
-    @tailrec
-    final def contractTrees(input: RoadNetTables, output: EdgeTable, s: Session): Unit = {
-        val leafEdgeQuery = for {
-            e <- input.roadNet
-            if !(Query(input.roadNet)).filter(x => x.start === e.start && x.end =!= e.end).exists
-        } yield e
+    def contractTrees(input: RoadNetTables, output: EdgeTable, s: Session) = {
+        @tailrec
+        def contractTreesInt(): Unit = {
+            val leafEdgeQuery = for {
+                e <- input.roadNet
+                if !(Query(input.roadNet)).filter(x => x.start === e.start && x.end =!= e.end).exists
+            } yield e
 
-        val newEdgeQuery = for {
-            l <- leafEdgeQuery
-            o <- output if o.end === l.start
-            n1 <- input.roadNodes if o.start === n1.id
-            n2 <- input.roadNodes if l.end === n2.id
-        } yield (o.start, l.end, l.cost + o.cost, false, (n1.geom shortestLine n2.geom).asColumnOf[Geometry])
+            val oneWayEdgeQuery = for {
+                e <- input.roadNet
+                if !(Query(input.roadNet)).filter(x => x.end === e.start && x.start === e.end).exists
+            } yield e
 
-        output.insert(newEdgeQuery)(s)
-        val cnt = leafEdgeQuery.delete(s)
-        if (cnt > 0)
-            contractTrees(input, output, s)
+            val newEdgeQuery = for {
+                l <- leafEdgeQuery
+                o <- output if o.end === l.start
+                n1 <- input.roadNodes if o.start === n1.id
+                n2 <- input.roadNodes if l.end === n2.id
+            } yield (o.start, l.end, l.cost + o.cost, false, (n1.geom shortestLine n2.geom).asColumnOf[Geometry])
+
+            output.insert(leafEdgeQuery)(s)
+            output.insert(newEdgeQuery)(s)
+
+            val cnt1 = leafEdgeQuery.delete(s)
+            val cnt2 = oneWayEdgeQuery.delete(s)
+            val cnt = cnt1 + cnt2
+            if (cnt > 0)
+                contractTreesInt()
+        }
+
+        contractTreesInt()
+
+        val isolatedNodes = for {
+            n <- input.roadNodes
+            if !Query(input.roadNet).filter(_.start === n.id).exists
+        } yield n.id
+
+        val toDel = output.filter(o => o.start.in(isolatedNodes) && o.end.in(isolatedNodes))
+        toDel.delete(s)
     }
 }
---
+++ src/test/scala/org/isochrone/hh/TreeContractionTest.scala
@@ -0,0 +1,25 @@
+package org.isochrone.hh
+
+import org.scalatest.FunSuite
+import org.isochrone.db.TestDatabase
+import org.isochrone.db.TestDatabaseComponent
+import org.isochrone.util.db.MyPostgresDriver.simple._
+import org.isochrone.db.DefaultRoadNetTablesWithPrefix
+import org.isochrone.db.EdgeTable
+
+class TreeContractionTest extends FunSuite with TestDatabase {
+    test("Tree contraction works") {
+        new TestDatabaseComponent {
+            val roadNetTables = new DefaultRoadNetTablesWithPrefix("tree_")
+            val output = new EdgeTable("tree_road_net_out")
+            database.withTransaction { implicit s: Session =>
+                object Contractor extends TreeContraction
+                Contractor.contractTrees(roadNetTables, output, s)
+                val got = Query(output).map(x => (x.start, x.end, x.cost)).to[Set]
+                assert(got === Set((4, 3, 1.0), (5, 3, 2.0), (6, 3, 3.0), (7, 3, 3.0)))
+                val got2 = Query(roadNetTables.roadNet).map(x => x.start -> x.end).to[Set]
+                assert(got2 === Set(1 -> 2, 2 -> 1, 1 -> 3, 3 -> 1, 2 -> 3, 3 -> 2))
+            }
+        }
+    }
+}
