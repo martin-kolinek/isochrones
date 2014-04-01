0e17f9e3d11cc1c60d565c8853990aab5a21e619
Martin Kolinek <martin.kolinek@gmail.com>
2014-04-01 23:25:19 +0200

    Added logging into roadimport

--- src/main/scala/org/isochrone/osm/RoadImporter.scala
+++ src/main/scala/org/isochrone/osm/RoadImporter.scala
@@ -9,11 +9,12 @@ import org.isochrone.ActionComponent
 import org.isochrone.ActionComponent
 import org.isochrone.db.EdgeTable
 import com.vividsolutions.jts.geom.Geometry
+import com.typesafe.scalalogging.slf4j.Logging
 
 trait RoadImporterComponent {
     self: OsmTableComponent with RoadNetTableComponent with DatabaseProvider with CostAssignerComponent =>
 
-    object roadImporter {
+    object roadImporter extends Logging {
         val roadNetQuery = {
             val roads = for {
                 w <- osmTables.ways
@@ -56,6 +57,7 @@ trait RoadImporterComponent {
                 en <- osmTables.nodes if en.id === e
             } yield (s, e, getRoadCost(sn.geom, en.geom), false, sn.geom.shortestLine(en.geom).asColumnOf[Geometry])
         }
+        logger.debug(s"roadNetQuery: ${roadNetQuery.selectStatement}")
 
         val backRoads = for {
             rn <- roadNetTables.roadNet
@@ -63,16 +65,21 @@ trait RoadImporterComponent {
             sn <- roadNetTables.roadNodes if sn.id === rn.start
             en <- roadNetTables.roadNodes if en.id === rn.end
         } yield (rn.end, rn.start, getNoRoadCost(sn.geom, en.geom), true, en.geom.shortestLine(sn.geom).asColumnOf[Geometry])
+        logger.debug(s"roadNetQuery: ${backRoads.selectStatement}")
 
         def execute() {
             database.withTransaction { implicit s: Session =>
                 import roadNetTables._
+                logger.debug("inserting roadNetQuery")
                 roadNet.insert(roadNetQuery)
+                logger.debug("inserting road_nodes")
                 sqlu"""INSERT INTO #${roadNetTables.roadNodes.baseTableRow.tableName}(id, region, geom)
                 SELECT DISTINCT n.id, 0, n.geom FROM #${roadNetTables.roadNet.baseTableRow.tableName} rn 
                     inner join #${osmTables.nodes.baseTableRow.tableName} n on rn.start_node = n.id OR rn.end_node = n.id
                 """.execute()
+                logger.debug("inserting backRoads")
                 roadNet.insert(backRoads)
+                logger.debug("inserting regions")
                 roadRegions.insert(0 -> Double.PositiveInfinity)
             }
         }
